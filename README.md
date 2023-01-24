84c27102095f0254fafe3f059bdc22185dfbc8a9
abcboy101 <16735361+abcboy101@users.noreply.github.com>
2023-01-24 00:24:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+An [equirectangular projection](https://en.wikipedia.org/wiki/Equirectangular_projection) map of the possible countries/regions corresponding to each [Vivillon](https://bulbapedia.bulbagarden.net/wiki/Vivillon_(Pokémon)) form.
+The dots indicate the latitude/longitude coordinates associated with each country/region in the Nintendo 3DS system software.
+Hatched countries/regions cannot be selected on a Nintendo 3DS system from any region.
+
+Map data for all countries except for France is from the [GADM database](https://gadm.org/data.html), version 4.1.
+Map data of France is from the GADM database, version 2.8.
+The data can be used for [non-commercial purposes](https://gadm.org/license.html) only.
+Data for Austria is licensed under the [CC BY-SA 2.0](https://creativecommons.org/licenses/by-sa/2.0/) license.
+
+The following country-level divisions in the GADM database are treated differently by the Nintendo 3DS system software:
+* [Greenland](https://en.wikipedia.org/wiki/Greenland) (considered a region of the [Kingdom of Denmark](https://en.wikipedia.org/wiki/Denmark))
+* [Ahvenanmaa / Åland](https://en.wikipedia.org/wiki/Åland) (considered a region of [Finland](https://en.wikipedia.org/wiki/Finland))
+* [French Guiana](https://en.wikipedia.org/wiki/French_Guiana), [Guadeloupe](https://en.wikipedia.org/wiki/Guadeloupe), [Martinique](https://en.wikipedia.org/wiki/Martinique), and [Réunion](https://en.wikipedia.org/wiki/Réunion) (considered regions of [France](https://en.wikipedia.org/wiki/France))
+* [Curaçao](https://en.wikipedia.org/wiki/Curaçao) (included as part of the [Netherlands Antilles](https://en.wikipedia.org/wiki/Netherlands_Antilles), which dissolved in 2010)
+* [Sint Maarten](https://en.wikipedia.org/wiki/Sint_Maarten) (included as part of the Netherlands Antilles, which dissolved in 2010)
+* [Bonaire](https://en.wikipedia.org/wiki/Bonaire), [Sint Eustatius](https://en.wikipedia.org/wiki/Sint_Eustatius), and [Saba](https://en.wikipedia.org/wiki/Saba) (included as part of the Netherlands Antilles, which dissolved in 2010)
+* [Svalbard and Jan Mayen](https://en.wikipedia.org/wiki/Svalbard_and_Jan_Mayen) (considered a region of [Norway](https://en.wikipedia.org/wiki/Norway) as "Svalbard")
+* [Serbia](https://en.wikipedia.org/wiki/Serbia) and [Kosovo](https://en.wikipedia.org/wiki/Kosovo) (combined into "Serbia and Kosovo")
+* [Puerto Rico](https://en.wikipedia.org/wiki/Puerto_Rico) (considered a region of the [United States](https://en.wikipedia.org/wiki/United_States))
+
+The following modifications to the subdivision data need to be made to match the Nintendo 3DS system software:
+* [France](https://en.wikipedia.org/wiki/France): Use the [regions](https://en.wikipedia.org/wiki/Regions_of_France) prior to 2015
+* [Trinidad and Tobago](https://en.wikipedia.org/wiki/Trinidad_and_Tobago): Use the [historic counties](https://en.wikipedia.org/wiki/Counties_of_Trinidad_and_Tobago) prior to the early 1990s
+* [Spain](https://en.wikipedia.org/wiki/Spain): Separate "Ceuta and Melilla" into [Ceuta](https://en.wikipedia.org/wiki/Ceuta) and [Melilla](https://en.wikipedia.org/wiki/Melilla)
+* [China](https://en.wikipedia.org/wiki/China): Treat [Hong Kong](https://en.wikipedia.org/wiki/Hong_Kong) and [Macao](https://en.wikipedia.org/wiki/Macau) as country-level divisions
+
+The following subdivisions in the GADM database are not used by the Nintendo 3DS system software:
+* [Antigua and Barbuda](https://en.wikipedia.org/wiki/Antigua_and_Barbuda): [Redonda](https://en.wikipedia.org/wiki/Redonda) (annexed by [Saint John](https://en.wikipedia.org/wiki/Saint_John_Parish,_Antigua_and_Barbuda) in 1872)
+* [Chile](https://en.wikipedia.org/wiki/Chile): [Arica y Parinacota](https://en.wikipedia.org/wiki/Arica_y_Parinacota_Region) (split from [Tarapacá](https://en.wikipedia.org/wiki/Tarapacá_Region) in 2007), [Los Ríos](https://en.wikipedia.org/wiki/Los_Ríos_Region) (split from [Los Lagos](https://en.wikipedia.org/wiki/Los_Lagos_Region) in 2007), and [Ñuble](https://en.wikipedia.org/wiki/Ñuble_Region) (split from [Bío-Bío](https://en.wikipedia.org/wiki/Biobío_Region) in 2017)
+* [Dominican Republic](https://en.wikipedia.org/wiki/Dominican_Republic): [San José de Ocoa](https://en.wikipedia.org/wiki/San_José_de_Ocoa_Province) (split from [Peravia](https://en.wikipedia.org/wiki/Peravia_Province) in 2000) and [Santo Domingo](https://en.wikipedia.org/wiki/Santo_Domingo_Province) (split from the [Distrito Nacional](https://en.wikipedia.org/wiki/Distrito_Nacional) in 2001)
+* [Greece](https://en.wikipedia.org/wiki/Greece): [Mount Athos](https://en.wikipedia.org/wiki/Monastic_community_of_Mount_Athos) (not included)
+* [India](https://en.wikipedia.org/wiki/India): [Arunachal Pradesh](https://en.wikipedia.org/wiki/Arunachal_Pradesh) (not included, likely due to [border dispute with China](https://en.wikipedia.org/wiki/Sino-Indian_border_dispute); India includes an "Other" region instead), [Jammu and Kashmir](https://en.wikipedia.org/wiki/Jammu_and_Kashmir_(state)) (not included, likely due to [border dispute with Pakistan](https://en.wikipedia.org/wiki/India–Pakistan_border) and China; India includes an "Other" region instead), and [Telangana](https://en.wikipedia.org/wiki/Telangana) (split from [Andhra Pradesh](https://en.wikipedia.org/wiki/Andhra_Pradesh) in 2014)
+* [Netherlands](https://en.wikipedia.org/wiki/Netherlands): [IJsselmeer](https://en.wikipedia.org/wiki/IJsselmeer) (body of water; borders [Flevoland](https://en.wikipedia.org/wiki/Flevoland), [Friesland](https://en.wikipedia.org/wiki/Friesland), and [North Holland](https://en.wikipedia.org/wiki/North_Holland)) and [Zeeuwse meren](https://en.wikipedia.org/wiki/Delta_Works) (body of water; borders [South Holland](https://en.wikipedia.org/wiki/South_Holland) and [Zeeland](https://en.wikipedia.org/wiki/Zeeland))
+* [New Zealand](https://en.wikipedia.org/wiki/New_Zealand): [Chatham Islands](https://en.wikipedia.org/wiki/Chatham_Islands) (not included)
+* [Nicaragua](https://en.wikipedia.org/wiki/Nicaragua): [Lake Nicaragua](https://en.wikipedia.org/wiki/Lake_Nicaragua) (body of water; borders [Granada](https://en.wikipedia.org/wiki/Granada_Department), [Rivas](https://en.wikipedia.org/wiki/Rivas_Department), and [Río San Juan](https://en.wikipedia.org/wiki/Río_San_Juan_Department))
+* [Panama](https://en.wikipedia.org/wiki/Panama): [Emberá-Wounaan](https://en.wikipedia.org/wiki/Emberá-Wounaan_Comarca) (split from [Darién](https://en.wikipedia.org/wiki/Darién_Province) in 1983), [Ngäbe-Buglé](https://en.wikipedia.org/wiki/Ngäbe-Buglé_Comarca) (split from portions of [Bocas del Toro](https://en.wikipedia.org/wiki/Bocas_del_Toro_Province), [Chiriquí](https://en.wikipedia.org/wiki/Chiriquí_Province), and [Veraguas](https://en.wikipedia.org/wiki/Veraguas_Province) in 1997), and [Panamá Oeste](https://en.wikipedia.org/wiki/Panamá_Oeste_Province) (split from [Panamá](https://en.wikipedia.org/wiki/Panamá_Province) in 2014)
+* [Peru](https://en.wikipedia.org/wiki/Peru): [Department of Lima](https://en.wikipedia.org/wiki/Department_of_Lima) (not included; [Lima Province](https://en.wikipedia.org/wiki/Lima_Province) is listed as "Lima")
+* [South Korea](https://en.wikipedia.org/wiki/South_Korea): [Sejong](https://en.wikipedia.org/wiki/Sejong_City) (split from [Gyeongsangnam-do](https://en.wikipedia.org/wiki/Gyeongsangnam-do) in 2012)
+
+The Nintendo 3DS system refers to [Tierra del Fuego](https://en.wikipedia.org/wiki/Tierra_del_Fuego_Province,_Argentina) and [Magallanes](https://en.wikipedia.org/wiki/Magallanes_Region) by their official names as "Tierra del Fuego, Antártida e Islas del Atlántico Sur" and "Magallanes y la Antártica Chilena", which would include disputed claims on the [Falkland Islands](https://en.wikipedia.org/wiki/Falkland_Islands), [South Georgia and the South Sandwich Islands](https://en.wikipedia.org/wiki/South_Georgia_and_the_South_Sandwich_Islands), and [Antarctica](https://en.wikipedia.org/wiki/Antarctica).
+These claims are not included on the map.
---
+++ make_map.py
@@ -0,0 +1,444 @@
+from __future__ import annotations
+
+import json
+import os
+import urllib.request
+import zipfile
+
+from concurrent.futures import ThreadPoolExecutor
+
+import geopandas as gpd
+import matplotlib.figure
+import matplotlib.patches
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+import shapely.affinity
+import shapely.errors
+import shapely.geometry
+import shapely.ops
+
+GPKG_DIR = "./gpkg/"
+FALLBACK = "Cannot be selected"
+categories = [
+    "Icy Snow Pattern",
+    "Polar Pattern",
+    "Tundra Pattern",
+    "Continental Pattern",
+    "Garden Pattern",
+    "Elegant Pattern",
+    "Meadow Pattern",
+    "Modern Pattern",
+    "Marine Pattern",
+    "Archipelago Pattern",
+    "High Plains Pattern",
+    "Sandstorm Pattern",
+    "River Pattern",
+    "Monsoon Pattern",
+    "Savanna Pattern",
+    "Sun Pattern",
+    "Ocean Pattern",
+    "Jungle Pattern",
+    FALLBACK,
+]
+cmap = matplotlib.colors.ListedColormap([
+    "#EDEDED",  # Icy Snow Pattern
+    "#004EA2",  # Polar Pattern
+    "#DDF1FB",  # Tundra Pattern
+    "#FAC71C",  # Continental Pattern
+    "#00893F",  # Garden Pattern
+    "#7967A7",  # Elegant Pattern
+    "#EE86AD",  # Meadow Pattern
+    "#E60020",  # Modern Pattern
+    "#00B0E4",  # Marine Pattern
+    "#AF5201",  # Archipelago Pattern
+    "#F39838",  # High Plains Pattern
+    "#D5C9A1",  # Sandstorm Pattern
+    "#C18700",  # River Pattern
+    "#838688",  # Monsoon Pattern
+    "#5AC7A5",  # Savanna Pattern
+    "#FFF462",  # Sun Pattern
+    "#9FD1F1",  # Ocean Pattern
+    "#543822",  # Jungle Pattern
+])
+
+
+def download_gadm() -> None:
+    """
+    Downloads all of the necessary GADM geospatial data files.
+    """
+    data_gadm = pd.read_csv('data_gadm.tsv', sep='\t', header=0)
+    futures = []
+    if not os.path.exists(GPKG_DIR):
+        os.mkdir(GPKG_DIR)
+    with ThreadPoolExecutor() as e:
+        for _, (code, url, level, old) in data_gadm.iterrows():
+            filepath = os.path.join(GPKG_DIR, os.path.basename(url))
+            if not os.path.exists(filepath):
+                futures.append(e.submit(urllib.request.urlretrieve, url, filepath))
+        for f in futures:
+            print(f'{f.result()[0]} downloaded')
+
+
+def get_data_from_file(code: str, url: str, level: int, old: bool, shift: int | float = 0) \
+        -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
+    """
+    Loads a GADM-style GPKG file from the local directory.
+
+    :param code: The ISO 3166-1 alpha-3 for the country
+    :param url: The original URL of the GADM-style GPKG file
+    :param level: The administrative subdivision level to load
+    :param old: Whether the file is from the GADM database, version 2.8
+    :param shift: The number of degrees of longitude to shift the geometry by
+    :return: A tuple of a GeoDataFrame of the subdivision data and a GeoDataFrame of the country-level data
+    """
+    filepath = os.path.join(GPKG_DIR, os.path.basename(url))
+    print(f'Loading {filepath}...')
+    if old:
+        with zipfile.ZipFile(filepath) as zf:
+            data = gpd.read_file(zf.open(f'{code}_adm.gpkg'), layer=f'{code}_adm{level}')
+            data_outline = gpd.read_file(zf.open(f'{code}_adm.gpkg'), layer=f'{code}_adm0')
+    elif level > 0:
+        data = gpd.read_file(filepath, layer=f'ADM_ADM_{level}')
+        data_outline = gpd.read_file(filepath, layer='ADM_ADM_0')
+    else:
+        data = gpd.read_file(filepath, layer='ADM_ADM_0')
+        data_outline = data.copy()
+
+    if shift != 0:
+        shift_map(data, shift)
+        shift_map(data_outline, shift)
+    return data, data_outline
+
+
+def load_gpkg_files(shift: int | float = 0) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
+    """
+    Loads all of the GADM-style GPKG files listed in data_gadm.tsv and concatenates them into a single GeoDataFrame.
+
+    :param shift: The number of degrees of longitude to shift the geometry by
+    :return: A tuple of a dataframe of the subdivision data and a dataframe of the country-level data
+    """
+    df: list[gpd.GeoDataFrame] = []
+    df_outline: list[gpd.GeoDataFrame] = []
+    data_gadm = pd.read_csv('data_gadm.tsv', sep='\t', header=0)
+    with ThreadPoolExecutor() as e:
+        futures = [e.submit(get_data_from_file, code, url, level, old, shift)
+                   for _, (code, url, level, old) in data_gadm.iterrows()]
+        for f in futures:
+            data, data_outline = f.result()
+            df.append(data)
+            df_outline.append(data_outline)
+    return pd.concat(df).reset_index(drop=True), pd.concat(df_outline).reset_index(drop=True)
+
+
+def load_areas_from_gpkg(shift: int | float = 0, save_pickle: bool = True) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
+    """
+    Loads all of the GPKG files and processes them.
+
+    :param shift: The number of degrees of longitude to shift the geometry by
+    :param save_pickle: Whether to save the processed geospatial data to a pickle file
+    :return: A tuple of a dataframe of the subdivision data and a dataframe of the country-level data
+    """
+    all_df, all_df_outline = load_gpkg_files(shift)
+
+    print("Reading area data...")
+    area_colors = pd.read_csv('data_areas.tsv', sep='\t', header=0)
+    assert len(all_df) == len(area_colors)
+    all_df['Category'] = area_colors['Category']
+
+    print("Merging areas...")
+    merge_all(all_df, all_df_outline)
+
+    print("Simplifying geometry...")
+    all_df['geometry'] = all_df['geometry'].simplify(0.025)  # approximately within half a pixel at the equator
+    all_df_outline['geometry'] = all_df_outline['geometry'].simplify(0.025)
+
+    if save_pickle:
+        if shift == 0:
+            all_df.to_pickle('all_df.pkl.gz', compression={'method': 'gzip', 'compresslevel': 1, 'mtime': 1})
+            all_df_outline.to_pickle('all_df_outline.pkl.gz', compression={'method': 'gzip', 'compresslevel': 1, 'mtime': 1})
+        else:
+            all_df.to_pickle(f'all_df_{shift}.pkl.gz', compression={'method': 'gzip', 'compresslevel': 1, 'mtime': 1})
+            all_df_outline.to_pickle(f'all_df_outline_{shift}.pkl.gz', compression={'method': 'gzip', 'compresslevel': 1, 'mtime': 1})
+    return all_df, all_df_outline
+
+
+def load_areas_from_pickle(shift: int | float = 0) -> tuple[gpd.GeoDataFrame, gpd.GeoDataFrame]:
+    """
+    Loads all of the GPKG files from a saved pickle.
+
+    :param shift: The number of degrees of longitude to shift the geometry by
+    :return: A tuple of a dataframe of the subdivision data and a dataframe of the country-level data
+    """
+    print("Loading pickles...")
+    if shift == 0:
+        all_df = pd.read_pickle('all_df.pkl.gz')
+        all_df_outline = pd.read_pickle('all_df_outline.pkl.gz')
+    else:
+        all_df = pd.read_pickle(f'all_df_{shift}.pkl.gz')
+        all_df_outline = pd.read_pickle(f'all_df_outline_{shift}.pkl.gz')
+    return all_df, all_df_outline
+
+
+def merge(df: gpd.GeoDataFrame, old: str, new: str, level: str = 'GID_1') -> None:
+    """
+    Merges two geometries into a single geometry in the specified dataframe. This is an in-place method.
+
+    :param df: The dataframe to modify
+    :param old: The value in the row that will be dropped after the merge
+    :param new: The value in the row that will be kept after the merge
+    :param level: The column with the values to match on
+    """
+    if level not in df.keys() or not any(df[level] == old):
+        print(f'Skipped: {old} -> {new} ({level})')
+    df.loc[df[level] == new, 'geometry'] = \
+        shapely.union_all([df[df[level] == old]['geometry'], df[df[level] == new]['geometry']])
+    df.drop(df[df[level] == old].index, inplace=True)
+
+
+def merge_all(all_df, all_df_outline) -> None:
+    """
+    Merges and removes various regions from the specified dataframe. This is an in-place method.
+
+    :param all_df: The dataframe of the subdivision data
+    :param all_df_outline: The dataframe of the country-level data
+    """
+    try:
+        # Don't include bodies of water
+        all_df.drop(all_df[(all_df['COUNTRY'] == 'Caspian Sea')
+                           | (all_df['GID_1'] == 'NLD.6_1')  # IJsselmeer
+                           | (all_df['GID_1'] == 'NLD.13_1')  # Zeeuwse meren
+                           | (all_df['GID_1'] == 'NIC.10_1')  # Nicaragua/Lago Nicaragua
+                           ].index, inplace=True)
+        all_df_outline.drop(all_df_outline[all_df_outline['COUNTRY'] == 'Caspian Sea'].index, inplace=True)
+        all_df_outline.loc[all_df_outline['GID_0'] == 'NLD', 'geometry'] = \
+            shapely.union_all(all_df[all_df['GID_0'] == 'NLD']['geometry'])
+        all_df_outline.loc[all_df_outline['GID_0'] == 'NIC', 'geometry'] = \
+            shapely.union_all(all_df[all_df['GID_0'] == 'NIC']['geometry'])
+    except KeyError:
+        pass
+
+    # Merge new areas
+    merge(all_df, "ATG.2_1", "ATG.4_1")  # Antigua and Barbuda/Redonda -> Antigua and Barbuda/Saint John
+    merge(all_df, "KOR.15_1", "KOR.3_1")  # South Korea/Sejong -> Chungcheongnam-do
+    merge(all_df, "CHL.4_1", "CHL.15_1")  # Chile/Arica y Parinacota -> Tarapacá
+    merge(all_df, "CHL.10_1", "CHL.9_1")  # Chile/Los Ríos -> Los Lagos
+    merge(all_df, "CHL.13_1", "CHL.6_1")  # Chile/Ñuble -> Bío-Bío
+    merge(all_df, "DOM.25_1", "DOM.20_1")  # Dominican Republic/San José de Ocoa -> Peravia
+    merge(all_df, "DOM.31_1", "DOM.5_1")  # Dominican Republic/Santo Domingo -> Distrito Nacional
+    # merge(all_df, "PAN.6_1", "PAN.5_1")  # Panama/Emberá -> Darién
+    # merge(all_df, "PAN.10_1", None)  # Panama/Ngöbe Buglé
+    merge(all_df, "PAN.11_1", "PAN.12_1")  # Panama/Panamá Oeste -> Panamá
+    merge(all_df, "IND.32_1", "IND.2_1")  # India/Telangana -> India/Andhra Pradesh
+
+    # Päijänne Tavastia is split across Eastern, Southern, and Western Finland, so merge them
+    merge(all_df, "FIN.4.3_1", "FIN.1.3_1", level="GID_2")  # Finland/Southern Finland/Päijänne Tavastia -> Finland/Eastern Finland/Päijänne Tavastia
+    merge(all_df, "FIN.5.5_1", "FIN.1.3_1", level="GID_2")  # Finland/Western Finland/Päijänne Tavastia -> Finland/Eastern Finland/Päijänne Tavastia
+
+    # Do not display disputed regions separately
+    merge(all_df, "ZNC", "CYP", level="GID_0")  # Northern Cyprus -> Cyprus
+    merge(all_df, "XAD", "CYP", level="GID_0")  # Akrotiri and Dhekelia -> Cyprus
+    # merge(all_df, "Z01.14_1", "IND.14_1")  # India/Jammu and Kashmir (all of it is disputed)
+    merge(all_df, "Z02.28_1", "CHN.28_1")  # China/Xinjiang Uygur
+    merge(all_df, "Z03.28_1", "CHN.28_1")  # China/Xinjiang Uygur
+    merge(all_df, "Z03.29_1", "CHN.29_1")  # China/Xijang
+    merge(all_df, "Z04.13_1", "IND.13_1")  # India/Himachal Pradesh
+    merge(all_df, "Z05.35_1", "IND.35_1")  # India/Uttarakhand
+    merge(all_df, "Z06", "PAK", level="GID_0")  # Pakistan (Siachen Glacier)
+    merge(all_df, "Z07.3_1", "IND.3_1")  # India/Arunachal Pradesh
+    merge(all_df, "Z08.29_1", "CHN.29_1")  # China/Xijang
+    merge(all_df, "Z09.13_1", "IND.13_1")  # India/Himachal Pradesh
+    merge(all_df, "Z09.35_1", "IND.35_1")  # India/Uttarakhand
+
+    merge(all_df_outline, "ZNC", "CYP", level="GID_0")  # Northern Cyprus -> Cyprus
+    merge(all_df_outline, "XAD", "CYP", level="GID_0")  # Akrotiri and Dhekelia -> Cyprus
+    merge(all_df_outline, "Z01", "IND", level="GID_0")  # India/Jammu and Kashmir
+    merge(all_df_outline, "Z02", "CHN", level="GID_0")  # China/Xinjiang Uygur
+    merge(all_df_outline, "Z03", "CHN", level="GID_0")  # China/Xinjiang Uygur, China/Xijang
+    merge(all_df_outline, "Z04", "IND", level="GID_0")  # India/Himachal Pradesh
+    merge(all_df_outline, "Z05", "IND", level="GID_0")  # India/Uttarakhand
+    merge(all_df_outline, "Z06", "PAK", level="GID_0")  # Pakistan (Siachen Glacier)
+    merge(all_df_outline, "Z07", "IND", level="GID_0")  # India/Arunachal Pradesh
+    merge(all_df_outline, "Z08", "CHN", level="GID_0")  # China/Xijang
+    merge(all_df_outline, "Z09", "IND", level="GID_0")  # India/Himachal Pradesh, India/Uttarakhand
+
+
+# https://stackoverflow.com/questions/58750837/set-centre-of-geopandas-map
+def shift_map(df: gpd.GeoDataFrame, shift: int | float) -> None:
+    """
+    Shifts all of the geometry in the dataframe by the specified number of degrees of longitude.
+    This is an in-place method.
+
+    :param df: The dataframe to shift
+    :param shift: The number of degrees of longitude to shift the geometry by
+    """
+    border = shapely.geometry.LineString([(shift - 180, 90), (shift - 180, -90)])
+    df["geometry"] = df["geometry"].apply(shift_map_helper, convert_dtype=False, border=border, shift=shift)
+
+
+def shift_map_helper(geo: shapely.Geometry, border: shapely.geometry.LineString, shift: int | float) \
+        -> shapely.Geometry:
+    """
+    Shifts a single piece of geometry in the dataframe by the specified number of degrees of longitude.
+    The piece of geometry will be split if it crosses the specified border,
+    and will be combined into a single piece if it was previously split.
+
+    :param geo: The piece of geometry to shift
+    :param border: The line of longitude marking the left and right edges of the map
+    :param shift: The number of degrees of longitude to shift the geometry by
+    :return: The shifted piece of geometry
+    """
+    split_row = shapely.ops.split(geo, border)
+    moved_map = []
+    for item in split_row.geoms:
+        minx, miny, maxx, maxy = item.bounds
+        if minx >= shift - 180:
+            moved_map.append(shapely.affinity.translate(item, xoff=-shift))
+        else:
+            moved_map.append(shapely.affinity.translate(item, xoff=360 - shift))
+    return shapely.union_all(moved_map)
+
+
+def make_map(*,
+             filename: str | bytes | os.PathLike = None,
+             language: str = 'en',
+             format: str = 'png',
+             shift: int | float = 0.0,
+             plot_squares: bool = True,
+             plot_areas: bool = True,
+             plot_points: bool = True,
+             save_pickle: bool = True,
+             load_pickle: bool = True) -> None:
+    """
+    Creates a map
+
+    :param filename: The filename that the generated map should be saved under
+    :param language: The language that should be used for the legend
+    :param format: The file format the generated map should be saved in
+    :param shift: The number of degrees of longitude to shift the geometry by
+    :param plot_squares: Whether to plot the underlying grid of squares
+    :param plot_areas: Whether to plot the countries and regions
+    :param plot_points: Whether to plot the coordinates for each region
+    :param save_pickle: Whether to save the processed geospatial data to a pickle file
+    :param load_pickle: Whether to load the processed geospatial data from a pickle file
+    """
+    print("Initializing plot...")
+    fig, ax = plt.subplots()
+    fig: matplotlib.figure.Figure
+    ax: matplotlib.figure.Axes
+
+    # Set font (the default matplotlib font does not have CJK characters)
+    plt.rcParams.update({'font.size': 36})
+    if language == 'ja':
+        plt.rcParams.update({'font.family': "Noto Sans JP"})
+    elif language == 'ko':
+        plt.rcParams.update({'font.family': "Noto Sans KR"})
+    elif language == 'zh-Hant':
+        plt.rcParams.update({'font.family': "Noto Sans TC"})
+    elif language == 'zh-Hans':
+        plt.rcParams.update({'font.family': "Noto Sans SC"})
+
+    # Set size and bounds of map
+    fig.set_size_inches(80, 40)
+    ax.set_aspect('equal')
+    plt.xlim([-180, 180])
+    plt.ylim([-90, 90])
+
+    # Legend with labels translated
+    categories_local = pd.read_csv('data_local.tsv', sep='\t', header=0)[language].tolist() + [FALLBACK]
+    handles = [matplotlib.patches.Patch(facecolor=color, edgecolor='black') for color in cmap.colors]
+    labels = categories_local[:-1]
+    ax.legend(handles, labels, loc='lower center', ncols=9)
+
+    # Display grid every 10 degrees
+    plt.xticks(np.arange(-180, 180 + 30, 30))
+    plt.yticks(np.arange(-90, 90 + 30, 30))
+    plt.xticks(np.arange(-180, 180 + 10, 10), minor=True)
+    plt.yticks(np.arange(-90, 90 + 10, 10), minor=True)
+    plt.grid(True, which='major', linewidth=0.5, color=(0.5, 0.5, 0.5, 0.5))
+    plt.grid(True, which='minor', linewidth=0.5, color=(0.5, 0.5, 0.5, 0.5))
+
+    # Hide axis ticks and labels
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+    ax.xaxis.set_ticks_position('none')
+    ax.yaxis.set_ticks_position('none')
+
+    if plot_squares:
+        print("Plotting squares...")
+        squares_arr = pd.read_csv('data_squares.tsv', sep='\t', header=None).values.tolist()
+        squares_geo = gpd.GeoSeries(
+            [shapely.geometry.box((x := (180 + 10 * j - 30) % 360 - 180),
+                                  (y := 90 - (10 * i)),
+                                  x + 10,
+                                  y - 10)
+             for i in range(len(squares_arr)) for j in range(len(squares_arr[i]))])
+        squares_cat = pd.Series([color for row in squares_arr for color in row])
+        squares_df = gpd.GeoDataFrame({
+            'geometry': squares_geo,
+            'Category': squares_cat,
+        })
+        if shift != 0:
+            shift_map(squares_df, shift)
+        squares_df.plot(ax=ax,
+                        column='Category',
+                        cmap=cmap,
+                        linewidths=0,
+                        edgecolors=(0.0, 0.0, 0.0, 0.0))
+
+    if plot_areas:
+        if not load_pickle:
+            all_df, all_df_outline = load_areas_from_gpkg(shift, save_pickle)
+        else:
+            all_df, all_df_outline = load_areas_from_pickle(shift)
+        
+        print("Plotting areas...")
+        all_df.loc[all_df['Category'] == FALLBACK].plot(
+            ax=ax,
+            linewidths=0,
+            hatch="///",
+            facecolor=(0.0, 0.0, 0.0, 0.0),
+            edgecolor=(0.0, 0.0, 0.0, 0.5)
+        )
+        ax = all_df.loc[all_df['Category'] != FALLBACK].plot(
+            ax=ax,
+            column='Category',
+            categories=categories[:-1],
+            cmap=cmap,
+            linewidths=0.3,
+            edgecolors='white')
+        all_df_outline.plot(
+            ax=ax,
+            linewidths=1,
+            facecolor=(0.0, 0.0, 0.0, 0.0),
+            edgecolor='black')
+
+    if plot_points:
+        print("Plotting points...")
+        with open('data_points.json', 'r', encoding='utf-8') as f:
+            points = [(division['latitude'], division['longitude'], division['form'])
+                      for country in json.load(f) for division in country['divisions']]
+        ax.scatter(x=[((row[1] + (360 - shift) + 180) % 360) - 180 for row in points],
+                   y=[row[0] for row in points],
+                   c=[row[2] for row in points],
+                   cmap=cmap,
+                   marker='.',
+                   linewidths=0.6,
+                   edgecolors='black',
+                   vmin=0,
+                   vmax=17)
+
+    print("Saving...")
+    plt.savefig(filename if filename is not None else f'map_{language}_{shift}.{format}',
+                format=format, bbox_inches='tight')
+
+    print("Done!")
+
+
+if __name__ == '__main__':
+    SHIFT = 0  # 0 for a map centered on the Prime Meridian, 150 for a map centered on the Pacific
+    download_gadm()
+    make_map(shift=SHIFT, language="en", save_pickle=True, load_pickle=False)
+    for LANGUAGE in ["de", "es", "fr", "it", "ja", "ko", "zh-Hant", "zh-Hans"]:
+        make_map(shift=SHIFT, language=LANGUAGE, save_pickle=False, load_pickle=True)
